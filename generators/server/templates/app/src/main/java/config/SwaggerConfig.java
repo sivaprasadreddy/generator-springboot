33418bd6ad3b1cfab045427d9e16d72f01322306
Raja Kolli <rajadilipkolli@gmail.com>
2022-10-02 04:33:23 +0530

    adds servers Item for running remotely
    Adding servers information allows user to run with both http and https  scheme especially when working in remote development like gitpod

--- generators/server/templates/app/src/main/java/config/SwaggerConfig.java
+++ generators/server/templates/app/src/main/java/config/SwaggerConfig.java
@@ -2,8 +2,17 @@ package <%= packageName %>.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "<%= appName %>", version = "v1"))
-public class SwaggerConfig {}
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addServersItem(new Server().url("/"));
+    }
+}
