bf913c40cb9fe29618115153e4aa21bf2a8d0879
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-13 21:33:09 +0530

    adds schema validation Test (#78)
    * adds schema validation Test
    
    
    https://vladmihalcea.com/validate-ddl-schema-spring-hibernate/ changes as per this
    
    * renames testname

---
+++ generators/server/templates/app/src/test/java/repository/SchemaValidationIntegrationTest.java
@@ -0,0 +1,12 @@
+package <%= packageName %>.repository;
+
+import <%= packageName %>.common.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestPropertySource;
+
+@TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
+public class SchemaValidationIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    void testSchemaValidity() {}
+}
