4925891771f7e3b4608c4d3602fa4deedd36fd97
Raja Kolli <rajadilipkolli@gmail.com>
2023-06-15 13:25:51 +0000

    fixes issue with generating project and refactor

--- generators/server/templates/app/src/main/java/exception/GlobalExceptionHandler.java
+++ generators/server/templates/app/src/main/java/exception/GlobalExceptionHandler.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class ErrorDetailProblemHandlingControllerAdvice {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
--- generators/server/templates/app/src/test/java/common/AbstractIntegrationTest.java
+++ generators/server/templates/app/src/test/java/common/AbstractIntegrationTest.java
@@ -8,14 +8,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles({PROFILE_TEST})
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {TestApplication.class})
 @AutoConfigureMockMvc
-@Import(TestApplication.class)
 public abstract class AbstractIntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
