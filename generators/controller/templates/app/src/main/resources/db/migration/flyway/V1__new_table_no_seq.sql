3db4c5faa8ca4983e9c2091547e99ce2359d857e
Raja Kolli <rajadilipkolli@gmail.com>
2023-08-30 10:19:15 +0530

    using text as column for postgresql tables (#124)
    * using text as column for postgresql tables
    
    as per good practices of postgres we should declare the data type as `text`
    
    This change address the issue
    
    * Including changes even for flyway

--- generators/controller/templates/app/src/main/resources/db/migration/flyway/V1__new_table_no_seq.sql
+++ generators/controller/templates/app/src/main/resources/db/migration/flyway/V1__new_table_no_seq.sql
@@ -1,5 +1,10 @@
 create table <%= tableName %> (
     id bigint not null auto_increment,
+    <%_ if (databaseType != 'postgresql') { _%>
     text varchar(1024) not null,
+    <%_ } _%>
+    <%_ if (databaseType === 'postgresql') { _%>
+    text text not null,
+    <%_ } _%>
     primary key (id)
 );
--- generators/controller/templates/app/src/main/resources/db/migration/liquibase/changelog/01-new_table_no_seq.sql
+++ generators/controller/templates/app/src/main/resources/db/migration/liquibase/changelog/01-new_table_no_seq.sql
@@ -4,6 +4,11 @@
 
 create table <%= tableName %> (
     id bigint not null auto_increment,
+    <%_ if (databaseType != 'postgresql') { _%>
     text varchar(1024) not null,
+    <%_ } _%>
+    <%_ if (databaseType === 'postgresql') { _%>
+    text text not null,
+    <%_ } _%>
     primary key (id)
 );
