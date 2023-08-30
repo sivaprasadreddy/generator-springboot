4eb128a4d507192077ffcb682dfc55c0da97b3c6
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-08-30 10:15:46 +0530

    V0.1.4 upgrades (#123)
    * Update docker.elastic.co/elasticsearch/elasticsearch Docker tag to v7.17.11
    
    * Update docker.elastic.co/kibana/kibana Docker tag to v7.17.11
    
    * Update docker.elastic.co/logstash/logstash Docker tag to v7.17.11
    
    * Update dependency gradle to v8.2
    
    * Update dependency gradle to v8.2.1
    
    * Update grafana/grafana Docker tag to v10.0.2
    
    * Update docker.elastic.co/elasticsearch/elasticsearch Docker tag to v7.17.12
    
    * Update docker.elastic.co/kibana/kibana Docker tag to v7.17.12
    
    * Update docker.elastic.co/logstash/logstash Docker tag to v7.17.12
    
    * Update eclipse-temurin Docker tag to v17.0.8_7-jre-focal
    
    * Update prom/prometheus Docker tag to v2.46.0
    
    * Update grafana/grafana Docker tag to v10.0.3
    
    * Update dependency maven to v3.9.4
    
    * Update dependency gradle to v8.3
    
    * Update grafana/grafana Docker tag to v10.0.4
    
    * Update grafana/grafana Docker tag to v10.1.0
    
    * Updating dependencies
    
    * Updating dependencies
    
    * Refine Testcontainers configuration
    
    * Simplify LocalStack configuration
    
    * Removed H2 database config
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- generators/server/templates/app/src/test/java/ApplicationIntegrationTest.java
+++ generators/server/templates/app/src/test/java/ApplicationIntegrationTest.java
@@ -2,9 +2,7 @@ package <%= packageName %>;
 
 import <%= packageName %>.common.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = {"spring.jpa.hibernate.ddl-auto=validate"})
 class ApplicationIntegrationTest extends AbstractIntegrationTest {
 
     @Test
--- generators/server/templates/app/src/test/java/common/AbstractIntegrationTest.java
+++ generators/server/templates/app/src/test/java/common/AbstractIntegrationTest.java
@@ -3,22 +3,15 @@ package <%= packageName %>.common;
 import static <%= packageName %>.utils.AppConstants.PROFILE_TEST;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import <%= packageName %>.TestApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-<%_ if (features.includes('localstack')) { _%>
-import org.springframework.test.context.ContextConfiguration;
-<%_ } _%>
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles({PROFILE_TEST})
-@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {TestApplication.class})
-<%_ if (features.includes('localstack')) { _%>
-@ContextConfiguration(initializers = {LocalStackConfig.class})
-<%_ } _%>
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = {ContainersConfig.class})
 @AutoConfigureMockMvc
 public abstract class AbstractIntegrationTest {
 
