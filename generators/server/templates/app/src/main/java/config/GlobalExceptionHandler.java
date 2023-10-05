71a0e8c0287c4451a6275cf9dd86495e929a50b9
Raja Kolli <rajadilipkolli@gmail.com>
2023-10-05 07:26:34 +0530

    Setting Precedence (#136)
    From 3.2.0-M2, without setting precedence. SB is not picking the configuration declared here.

--- generators/server/templates/app/src/main/java/config/GlobalExceptionHandler.java
+++ generators/server/templates/app/src/main/java/config/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package <%= packageName %>.config;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
