6cef336f2fdd47361aaa149e21a07e1819bef02f
Raja Kolli <rajadilipkolli@gmail.com>
2023-11-04 09:38:38 +0530

    fixes issue with getter

--- generators/server/templates/app/src/main/java/exception/ResourceNotFoundException.java
+++ generators/server/templates/app/src/main/java/exception/ResourceNotFoundException.java
@@ -1,13 +1,15 @@
 package <%= packageName %>.exception;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
 public class ResourceNotFoundException extends RuntimeException {
 
     private static final HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 
     public ResourceNotFoundException(String errorMessage) {
         super(errorMessage);
+    }
+    
+    public HttpStatusCode getHttpStatus() {
+        return httpStatus;
     }
 }
